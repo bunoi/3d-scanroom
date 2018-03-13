e48a9de2258a03c5b5338561832bfee3d893c214
bunoi <taobunoi@gmail.com>
2018-03-14 04:26:52 +0700

    Rotation matrix for rotate plane

--- main.cpp
+++ main.cpp
@@ -15,13 +15,36 @@
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 
+#include <Eigen/Dense>
+
 #include "tinyply.h"
 
 using namespace tinyply;
 using namespace std;
 using namespace cv;
+using namespace Eigen;
+using namespace pcl;
 
-void read_ply_file(const std::string & filename, vector<float> &verts, vector<uint32_t> &faces)
+const int laser_slider_max = 200;
+int laser_slider;
+double laser;
+
+const float resolution = 0.01; // meters per pixel
+float offsetx = 0, offsetz = 0;
+
+int height,width;
+uint32_t vertexCount, faceCount;
+
+const bool enTransform = true;
+
+vector<PointXYZ> verts;
+vector<uint32_t> faces;
+
+Mat fake;
+
+int occ[1000][1000];
+
+void read_ply_file(const std::string & filename)
 {
 	// Tinyply can and will throw exceptions at you!
 	try
@@ -29,6 +52,7 @@ void read_ply_file(const std::string & filename, vector<float> &verts, vector<ui
 		// Read the file and create a std::istringstream suitable
 		// for the lib -- tinyply does not perform any file i/o.
 		std::ifstream ss(filename);
+		vector<float> points;
 
 		// Parse the ASCII header fields
 		PlyFile file(ss);
@@ -51,10 +75,19 @@ void read_ply_file(const std::string & filename, vector<float> &verts, vector<ui
 		uint32_t vertexCount, faceCount;
 		vertexCount = faceCount = 0;
 
-		vertexCount = file.request_properties_from_element("vertex", { "x", "y", "z" }, verts);
+		vertexCount = file.request_properties_from_element("vertex", { "x", "y", "z" }, points);
 		faceCount = file.request_properties_from_element("face", { "vertex_indices" }, faces, 3);
 
 		file.read(ss);
+
+		for ( int i = 0; i < points.size(); i+=3 )
+		{
+			PointXYZ p;
+			p.x = points[i];
+			p.y = points[i+1];
+			p.z = points[i+2];
+			verts.push_back(p);
+		}
 
 		std::cout << "\tRead " << verts.size() << " total vertices (" << vertexCount << " properties)." << std::endl;
 		std::cout << "\tRead " << faces.size() << " total faces (triangles) (" << faceCount << " properties)." << std::endl;
@@ -67,24 +100,6 @@ void read_ply_file(const std::string & filename, vector<float> &verts, vector<ui
 	}
 }
 
-const int laser_slider_max = 200;
-int laser_slider;
-double laser;
-
-float floor_height = 1.6;
-const float resolution = 0.01; // meters per pixel
-float offsetx = 0, offsetz = 0;
-
-int height,width;
-uint32_t vertexCount, faceCount;
-
-vector<float> verts;
-vector<uint32_t> faces;
-
-Mat fake;
-
-int occ[1000][1000];
-
 void on_trackbar( int , void *)
 {
 	laser = (float) laser_slider / 100;
@@ -96,10 +111,10 @@ void on_trackbar( int , void *)
 	cout << offsetx << endl;
 	for ( int i = 0; i < vertexCount; ++i)
 	{
-		if ( verts[3*i + 1] - floor_height > 0.1)	continue;
+		if ( verts[i].y < 0)	continue;
 
-		float x = verts[3*i] + offsetx;
-		float z = verts[3*i + 2] + offsetz;
+		float x = verts[i].x + offsetx;
+		float z = verts[i].z + offsetz;
 
 		int r = (int) (x / resolution);
 		int c = (int) (z / resolution);
@@ -108,12 +123,12 @@ void on_trackbar( int , void *)
 		{
 			cout << r << " " << c << endl;
 		}
-		if (abs(floor_height - verts[3*i + 1] - laser) < 0.1 )
+		if (abs(verts[i].y - laser) < 0.1 )
 		{
 			occ[r][c] = 2;
 			//cout << x << " " << z << " " << r << " " << c << endl;
 		}
-		else if (occ[r][c] == 0 and abs(floor_height - verts[3*i + 1]) < 0.05 )
+		else if (occ[r][c] == 0 and verts[i].y < 0.05 )
 		{
 			occ[r][c] = 1;
 		}
@@ -135,22 +150,23 @@ void on_trackbar( int , void *)
 
 }
 // plane -0.00455174 -0.988815 0.149075 1.78232
-void cut_plane(vector<float> &all_point, vector<float> &remain)
+//0.13184 1.5674 -0.94338
+//0.99496 2.083 1.8732
+void cut_plane(vector<PointXYZ> &all_point, vector<PointXYZ> &remain)
 {
 	remain.clear();
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-	cloud->width  = all_point.size() / 3;
+	cloud->width  = all_point.size();
 	cloud->height = 1;
 	cloud->points.resize (cloud->width * cloud->height);
 
 	// Generate the data
 	for (size_t i = 0; i < cloud->points.size (); ++i)
 	{
-		cloud->points[i].x = all_point[3*i];
-		cloud->points[i].y = all_point[3*i + 1];
-		cloud->points[i].z = all_point[3*i + 2];
+		cloud->points[i] = all_point[i];
 	}
+
 	pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
 	pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
 	// Create the segmentation object
@@ -170,10 +186,20 @@ void cut_plane(vector<float> &all_point, vector<float> &remain)
                                       << coefficients->values[2] << " " 
                                       << coefficients->values[3] << std::endl;
 
+	// Print Origin and another point
+
+	cout << all_point[inliers->indices[0]].x << " "
+		<< all_point[inliers->indices[0]].y << " "
+		<< all_point[inliers->indices[0]].z << endl;
+
+	cout << all_point[inliers->indices[inliers->indices.size()-1]].x << " "
+		<< all_point[inliers->indices[inliers->indices.size()-1]].y << " "
+		<< all_point[inliers->indices[inliers->indices.size()-1]].z << endl;
+
 	pcl::PointCloud<pcl::PointXYZ>::Ptr new_cloud(new pcl::PointCloud<pcl::PointXYZ>);
 	pcl::PointCloud<pcl::PointXYZ>::Ptr plane(new pcl::PointCloud<pcl::PointXYZ>);
 
-	new_cloud->width  = all_point.size() / 3 - inliers->indices.size();
+	new_cloud->width  = all_point.size() - inliers->indices.size();
 	new_cloud->height = 1;
 	new_cloud->points.resize (new_cloud->width * new_cloud->height);
 
@@ -193,14 +219,48 @@ void cut_plane(vector<float> &all_point, vector<float> &remain)
 		{
 			new_cloud->points[p] = cloud->points[i];
 			p++;
-			remain.push_back(cloud->points[i].x);
-			remain.push_back(cloud->points[i].y);
-			remain.push_back(cloud->points[i].z);
+			remain.push_back(cloud->points[i]);
 		}
 	}
 
 	pcl::io::savePCDFileASCII ("deleted_plane.pcd", *new_cloud);
 	pcl::io::savePCDFileASCII ("plane.pcd", *plane);
+}
+
+void findRotationMatrix(vector<float> &equation, PointXYZ origin, PointXYZ pointInPlane, Matrix4f &mat)
+{
+	Matrix4f wTOF;
+	
+	// Find X axis
+	Vector3f x(pointInPlane.x - origin.x, pointInPlane.y - origin.y, pointInPlane.z - origin.z);
+	x = x / x.norm();
+
+	// Find Y axis
+	Vector3f y(equation[0], equation[1], equation[2]);
+	y = y / y.norm();
+
+	Vector3f z;
+	z = x.cross(y);
+	z = z / z.norm();
+
+	cout << x << endl;
+	cout << y << endl;
+	cout << z << endl;
+
+	wTOF.block(0,0,3,1) = x;
+	wTOF.block(0,1,3,1) = y;
+	wTOF.block(0,2,3,1) = z;
+	wTOF(0,3) = origin.x;
+	wTOF(1,3) = origin.y;
+	wTOF(2,3) = origin.z;
+	wTOF(3,3) = 1;
+	wTOF(3,0) = wTOF(3,1) = wTOF(3,2) = 0;
+
+	cout << wTOF << endl;
+
+	mat = wTOF.inverse();
+
+	cout << mat << endl;
 }
 
 int main (int argc, char** argv)
@@ -209,32 +269,64 @@ int main (int argc, char** argv)
 	if (argc < 2 )	return 0;
 	
 	string fileName = argv[1];	
-    
-    read_ply_file(fileName, verts, faces);
+  
+    read_ply_file(fileName);
 
     std::cout << "\tRead " << verts.size() << " total vertices (" << std::endl;
 	std::cout << "\tRead " << faces.size() << " total faces (triangles) (" << std::endl;
 
-	
-	vertexCount = verts.size() / 3;
+	if (enTransform)
+	{
+		// plane -0.00455174 -0.988815 0.149075 1.78232
+		//0.13184 1.5674 -0.94338
+		//0.99496 2.083 1.8732
+		PointXYZ origin(0.13184, 1.5674, -0.94338),p(0.99496, 2.083, 1.8732);
+		vector<float> equation(5);
+		equation[0] = -0.00455174;
+		equation[1] = -0.988815;
+		equation[2] = 0.149075;
+		equation[3] = 1.78232;
 
-	float ground = 10, top = 0;
+		Matrix4f R;
+		findRotationMatrix(equation, origin, p, R);
+
+		for (int i=0; i < verts.size(); i++ )
+		{
+			Vector4f p(verts[i].x, verts[i].y, verts[i].z, 1);
+			p = R * p;
+			verts[i].x = p[0];
+			verts[i].y = p[1];
+			verts[i].z = p[2];
+		}
+
+		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+		cloud->width  = verts.size();
+		cloud->height = 1;
+		cloud->points.resize (cloud->width * cloud->height);
+
+		// Generate the data
+		for (size_t i = 0; i < cloud->points.size (); ++i)
+		{
+			cloud->points[i] = verts[i];
+		}
+
+		pcl::io::savePCDFileASCII ("roteted_mao.pcd", *cloud);
+	}
+	
+	vertexCount = verts.size();
+
 
 	float sizex = 0, sizez = 0;
 
 	for ( int i = 0; i < vertexCount; ++i)
 	{
-		ground = min( ground, verts[3*i+1]);
-		top = max(top, verts[3*i+1]);
 
-		offsetx = min(offsetx, verts[3*i]);
-		sizex   = max(sizex, verts[3*i]);
-		offsetz = min(offsetz, verts[3*i + 2]);
-		sizez   = max(sizez, verts[3*i + 2]);
+		offsetx = min(offsetx, verts[i].x);
+		sizex   = max(sizex, verts[i].x);
+		offsetz = min(offsetz, verts[i].z);
+		sizez   = max(sizez, verts[i].z);
 	}
-
-	std::cout << "The minimum point is " << ground << std::endl;
-	std::cout << "The highest point is " << top << std::endl;
 
 	offsetx *= -1;
 	offsetz *= -1;
@@ -264,7 +356,7 @@ int main (int argc, char** argv)
 	cout << " Track bar Complete" << endl;
  	/// Show some stuff
  	on_trackbar( laser_slider, 0 );
-	vector<float> all,remain;
+	vector<PointXYZ> all,remain;
 	all = verts;
 	while(true)
 	{
